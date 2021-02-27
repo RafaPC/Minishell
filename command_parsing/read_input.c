048d282eb0590a3369199861824143ab0d9598f4
aiglesia <Ingoleth@github.com>
2021-02-27 01:59:45 +0100

    Fix memory leak

--- command_parsing/read_input.c
+++ command_parsing/read_input.c
@@ -6,7 +6,7 @@
 /*   By: aiglesia <aiglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 21:07:22 by aiglesia          #+#    #+#             */
-/*   Updated: 2021/02/27 01:41:35 by aiglesia         ###   ########.fr       */
+/*   Updated: 2021/02/27 01:57:47 by aiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,21 @@ void	read_input(char **line)
 	char	buffer[BUFFER_SIZE + 1];
 	char	rd_buffer[2];
 	int		i;
-	int		bytes_read;
+	char	*aux;
 
 	i = 0;
 	rd_buffer[1] = 0;
 	*line = 0;
 	buffer[BUFFER_SIZE] = 0;
-	while ((bytes_read = read(STDIN_FILENO, rd_buffer, 1)))
+	while (read(STDIN_FILENO, rd_buffer, 1))
 	{
 		if (rd_buffer[0] == '\n')
 			break ;
 		if (i == BUFFER_SIZE)
 		{
+			aux = *line;
 			*line = ft_strncat_in(*line, (char *)&buffer, BUFFER_SIZE);
+			free (aux);
 			i = 0;
 		}
 		buffer[i] = rd_buffer[0];
