ec38fd2a70ee3f170a4c9ae76d1bb4304de0f544
RafaPC <rafitap.c@hotmail.com>
2021-03-31 15:32:37 +0200

    Changes so that is debuggable

---
+++ debug/debug_minishell.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug_minishell.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rprieto- <rprieto-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/09 11:28:26 by aiglesia          #+#    #+#             */
+/*   Updated: 2021/03/24 18:56:17 by rprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	print_command_table(t_command *command)
+{
+	char	**token_reader;
+
+	while (true)
+	{
+		token_reader = command->tokens;
+		ft_printf(STDOUT_FILENO,
+			"Comando: %s\n\nRelacion: %i\n\nArgumentos:\n\n",
+			token_reader[0], command->relation);
+		while ((++token_reader)[0])
+			ft_printf(STDOUT_FILENO, "%15.d%c %s\n", 0, '*', token_reader[0]);
+		ft_printf(STDOUT_FILENO, "\nOutput:\n\n");
+		if (command->relation == 0)
+			break ;
+		command = command->next;
+	}
+}
+
+void	iterate_through_commands(t_gnl_buffer *gnl_buff,
+t_shell *shell, t_bool verbose)
+{
+	shell->commands = NULL;
+	while (gnl_buff)
+	{
+		shell->buffer = gnl_buff->line;
+		ft_printf(STDOUT_FILENO, "Input: \"%s\"\n\n", shell->buffer);
+		if (!print_parsing_error(
+			split_commands(shell), &shell->prev_exit_status))
+		{
+			while (shell->commands)
+			{
+				if (verbose)
+					print_command_table(shell->commands);
+				shell->commands = execute_commands(shell);
+			}
+		}
+		gnl_buff = gnl_buff->next;
+		ft_printf(STDOUT_FILENO, "---\n");
+	}
+}
+
+void	debug_minishell(t_shell *shell, t_bool verbose)
+{
+	int				fd;
+	t_gnl_buffer	*gnl_buff;
+
+	gnl_buff = NULL;
+	fd = open("command_list", O_RDONLY);
+	if (fd != -1)
+	{
+		gnl_buffer(fd, 0, &gnl_buff);
+		iterate_through_commands(gnl_buff, shell, verbose);
+		free_gnl_buffer(gnl_buff, false);
+		close(fd);
+	}
+	exit_command(shell); //double check
+}
--- main.c
+++ main.c
@@ -6,35 +6,51 @@
 /*   By: rprieto- <rprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 19:16:07 by aiglesia          #+#    #+#             */
-/*   Updated: 2021/03/22 01:20:36 by rprieto-         ###   ########.fr       */
+/*   Updated: 2021/03/31 15:27:27 by rprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 
 int		main(int argc, char **argv, const char **env)
 {
 	t_shell		shell;
 
 	shell.prev_exit_status = errno;
-	shell.env_list = create_env_list(env, argv[0]);
 	shell.command_history = NULL;
 	shell.commands = NULL;
 	shell.copy_line = NULL;
-	while (true)
+	if (argc == 1)
 	{
-		if (!handle_input(&shell.buffer, &shell.command_history,
-			&shell.copy_line))
+		shell.env_list = create_env_list(env, argv[0]);
+		while (true)
 		{
-			write(STDOUT_FILENO, "exit\n", 5);
-			free_and_exit(&shell, 130);
-		}
-		if (!print_parsing_error(split_commands(&shell),
-			&shell.prev_exit_status))
-		{
-			while (shell.commands)
-				shell.commands = execute_commands(&shell);
+			if (!handle_input(&shell.buffer, &shell.command_history, &shell.copy_line))
+			{
+				write(STDOUT_FILENO, "exit\n", 5);
+				free_and_exit(&shell, 130);
+			}
+			if (!print_parsing_error(split_commands(&shell), &shell.prev_exit_status))
+			{
+				while (shell.commands)
+					shell.commands = execute_commands(&shell);
+			}
 		}
 	}
-	argc++;
+	else if (argc == 2)
+	{
+		ft_lstclear(&shell.env_list, free);
+		shell.env_list = create_env_list((const char**)get_false_env_array(), argv[0]);
+		debug_minishell(&shell, !ft_strncmp(argv[1], "-v", 3));
+	}
+	else //PARA EL TESTER, COGE EL INPUT POR EL ARGUMENTO
+	{
+		shell.env_list = create_env_list(env, argv[0]);
+		shell.buffer = ft_strdup(argv[argc - 1]);
+		if (!print_parsing_error(split_commands(&shell), &shell.prev_exit_status))
+			while (shell.commands)
+				shell.commands = execute_commands(&shell);
+		return (shell.prev_exit_status);
+	}
 }
